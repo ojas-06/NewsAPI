7eed5e00d1eef50532f8b732288cf40b9a5f7d0f
Ojas Deshpande <ojas.md2006@gmail.com>
2026-06-03 08:43:43 +0000

    dockerised, migrating to self hosted server on OC

--- src/index.js
+++ src/index.js
@@ -7,8 +7,29 @@ import { view } from "./routes/view.js";
 import test from "./routes/root.js";
 import { search } from "./routes/search.js";
 
+const allowedOrigins = [
+  'https://news-mern.netlify.app',
+  'http://localhost:5173',
+  'http://localhost:5173/',
+  'http://127.0.0.1:5173',
+  'http://127.0.0.1:5173/'                    
+];
+
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+  }
+}));
+
 const PORT = process.env.PORT || 3000;
 
 connectDB();
